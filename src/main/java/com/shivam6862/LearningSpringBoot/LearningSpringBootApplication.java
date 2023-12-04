cdcee87745ecfa357fa08822ad0f7f0f3b010c31
shivam6862 <shivam6862mau@gmail.com>
2023-12-05 01:54:21 +0530

    starter kit

---
+++ src/main/java/com/shivam6862/LearningSpringBoot/LearningSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.shivam6862.LearningSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shivam6862/LearningSpringBoot/LearningSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam6862.LearningSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
